ff9435b0cd2efacfe3fb1661ee0b4df586e03063
Tim Köhler <33375734+timksf@users.noreply.github.com>
2025-12-15 20:16:05 +0100

    fpga build update

---
+++ hdl/src/synth.tcl
@@ -0,0 +1,31 @@
+# synth
+synth_design -top mkFPGATestSimpleTop
+puts "Finished synth_design"
+
+# implement
+opt_design
+
+report_timing_summary -file $project_dir/${project_name}_tim1.rpt
+report_utilization -file $project_dir/${project_name}_util1.rpt
+report_drc -file $project_dir/${project_name}_drc1.rpt
+puts "Finished opt_design"
+
+place_design
+
+report_timing_summary -file $project_dir/${project_name}_tim2.rpt
+report_utilization -file $project_dir/${project_name}_util2.rpt
+puts "Finished place_design"
+
+route_design
+puts "Finished route_design"
+
+phys_opt_design
+puts "Finished phys_opt_design"
+
+report_timing_summary -file $project_dir/${project_name}_tim3.rpt
+report_utilization -file $project_dir/${project_name}_util3.rpt
+report_drc -file $project_dir/${project_name}_drc3.rpt
+
+write_checkpoint $project_dir/${project_name}_chkpt.dcp -force
+
+write_bitstream -file 
